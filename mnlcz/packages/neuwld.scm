00b44c04e4bb33f895ac69078d5df3a393d365e9
mnlcz <mnlcz@tuta.io>
2026-06-20 20:40:37 -0300

    Fix imports

--- mnlcz/packages/neuwld.scm
+++ mnlcz/packages/neuwld.scm
@@ -3,6 +3,7 @@
   #:use-module (guix download)
   #:use-module (guix build-system meson)
   #:use-module (guix licenses)
+  #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages fontutils)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages graphics)
@@ -13,21 +14,16 @@
     (name "neuwld")
     (version "0.0")
     (source
-      (origin
-        (method url-fetch)
-        (uri "https://git.sr.ht/~shrub900/neuwld/archive/bb5d247.tar.gz")
-        (sha256
-          (base32
-            "0f0zcv0av6jq8ih6gfvfgsyyp59sdkrsr0najr55imalji9m54gl"))))
+     (origin
+       (method url-fetch)
+       (uri "https://git.sr.ht/~shrub900/neuwld/archive/bb5d247.tar.gz")
+       (sha256
+        (base32 "0f0zcv0av6jq8ih6gfvfgsyyp59sdkrsr0najr55imalji9m54gl"))))
     (build-system meson-build-system)
-    (inputs
-      (list fontconfig
-            pixman
-            freetype))
-    (propagated-inputs
-      (list wayland
-            libdrm))
+    (native-inputs (list pkg-config))
+    (inputs (list fontconfig pixman freetype wayland libdrm))
     (synopsis "Primitive drawing library targeted at Wayland")
-    (description "neuwld is a primitive drawing library targeted at Wayland compositors.")
+    (description
+     "neuwld is a primitive drawing library targeted at Wayland compositors.")
     (home-page "https://git.sr.ht/~shrub900/neuwld")
     (license expat)))
