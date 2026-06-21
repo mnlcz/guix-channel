3930f2977fa7a1c3fde2ea52c7524033a8b6b344
mnlcz <mnlcz@tuta.io>
2026-06-21 19:57:50 -0300

    Add initial mojito package

---
+++ mnlcz/packages/mojito.scm
@@ -0,0 +1,34 @@
+(define-module (mnlcz packages mojito)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix licenses)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages freedesktop)
+  #:use-module (mnlcz packages neuwld)
+  #:use-module (mnlcz packages neuswc))
+
+(define-public mojito
+  (package
+    (name "mojito")
+    (version "4c6b988")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        "https://git.sr.ht/~dlm/mojito/archive/4c6b988e1927cff3bd6ffe22c552c31cd356dfca.tar.gz")
+       (sha256
+        (base32 "140l4fppcc0d9bbqlgrhaskphsmshyicab4sbjndx8x2rn10gpjs"))))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:tests? #f
+       #:make-flags (list (string-append "PREFIX=" %output))
+       #:phases (modify-phases %standard-phases
+                  (delete 'configure))))
+    (native-inputs (list pkg-config wayland))
+    (inputs (list wayland neuwld neuswc))
+    (synopsis "Featherweight lemonbar-compatible bar for Wayland")
+    (description
+     "mojito is a minimal, lemonbar-compatible status bar for Wayland compositors using neuswc.")
+    (home-page "https://git.sr.ht/~dlm/mojito")
+    (license bsd-0)))
