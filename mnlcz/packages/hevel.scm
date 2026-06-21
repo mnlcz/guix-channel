5931e8470040c0b6997bb69e6c01b98c7514d0c0
mnlcz <mnlcz@tuta.io>
2026-06-21 02:14:03 -0300

    Fix

--- mnlcz/packages/hevel.scm
+++ mnlcz/packages/hevel.scm
@@ -25,14 +25,19 @@
         (base32 "0z01s825vhkylmfqhr3whj9pds6w9vys7yb3s4ja208yx6x3l0d2"))))
     (build-system gnu-build-system)
     (arguments
-     '(#:tests? #f
-       #:make-flags (list "CC=gcc"
-                          (string-append "PREFIX=" %output))
-       #:phases (modify-phases %standard-phases
-                  (delete 'configure)
-                  (add-before 'build 'copy-config
-                    (lambda _
-                      (copy-file #$(local-file "../../config/hevel-config.h") "config.h"))))))
+     (list
+      #:tests? #f
+      #:make-flags
+      #~(list "CC=gcc"
+              (string-append "PREFIX="
+                             #$output))
+      #:phases
+      #~(modify-phases %standard-phases
+          (delete 'configure)
+          (add-before 'build 'copy-config
+            (lambda _
+              (copy-file #$(local-file "../../config/hevel-config.h")
+                         "config.h"))))))
     (native-inputs (list pkg-config))
     (inputs (list wayland
                   libdrm
