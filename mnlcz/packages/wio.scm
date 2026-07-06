c35218ae0f438fd6964cb4d0381a853f162c3d53
mnlcz <mnlcz@tuta.io>
2026-07-06 08:14:27 -0300

    add temp patch for debug

--- mnlcz/packages/wio.scm
+++ mnlcz/packages/wio.scm
@@ -33,7 +33,9 @@
 		      (local-file
 			"/home/mnlcz/Projects/guix-channel/patches/wio-output-state-reinit-0.20.patch")
 		      (local-file
-			"/home/mnlcz/Projects/guix-channel/patches/wio-input-cursorbtn-0.20.patch")))))
+			"/home/mnlcz/Projects/guix-channel/patches/wio-input-cursorbtn-0.20.patch")
+		      (local-file
+			"/home/mnlcz/Projects/guix-channel/patches/wio-input-dbg.patch")))))
     (build-system meson-build-system)
     (arguments
      (list
