99b2fff1fc734942262c84d260329c0e5a7b436a
mnlcz <mnlcz@tuta.io>
2026-07-11 08:53:51 -0300

    cage: attempt wlroots patch for destroy crash

--- mnlcz/packages/cage.scm
+++ mnlcz/packages/cage.scm
@@ -3,7 +3,8 @@
   #:use-module (guix download)
   #:use-module (guix gexp)
   #:use-module (guix build-system meson)
-  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module ((guix licenses)
+                #:prefix license:)
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages freedesktop)
   #:use-module (gnu packages xdisorg)
@@ -17,11 +18,14 @@
     (source
      (origin
        (method url-fetch)
-       (uri "https://github.com/cage-kiosk/cage/archive/refs/tags/v0.2.1.tar.gz")
+       (uri
+        "https://github.com/cage-kiosk/cage/archive/refs/tags/v0.2.1.tar.gz")
        (sha256
         (base32 "1a9l28cgfckw4vf6zzvs37vw9gfyrcw97y5rsy4afr2i2y1hraxc"))
        (patches (list (local-file
-                "/home/mnlcz/Projects/guix-channel/patches/cage-xwayland-cursor-0.20.patch")))))
+                       "/home/mnlcz/Projects/guix-channel/patches/cage-xwayland-cursor-0.20.patch")
+                      (local-file
+                       "/home/mnlcz/Projects/guix-channel/patches/cage-xwayland-destroy-0.20.patch")))))
     (build-system meson-build-system)
     (arguments
      (list
@@ -34,7 +38,8 @@
                 (("wlroots-0\\.19")
                  "wlroots-0.20")))))))
     (native-inputs (list pkg-config))
-    (inputs (list wayland (replace-mesa wlroots) libxkbcommon))
+    (inputs (list wayland
+                  (replace-mesa wlroots) libxkbcommon))
     (synopsis "Wayland kiosk compositor")
     (description "Cage is a kiosk compositor for Wayland.")
     (home-page "https://github.com/cage-kiosk/cage")
