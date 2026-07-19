5da130f994609b6b3bf533ff6ce2ad865a26f847
mnlcz <mnlcz@tuta.io>
2026-07-19 04:38:16 -0300

    guix style

--- mnlcz/packages/wio.scm
+++ mnlcz/packages/wio.scm
@@ -38,8 +38,8 @@
                        "/home/mnlcz/Projects/guix-channel/patches/wio-input-dbg.patch")
                       (local-file
                        "/home/mnlcz/Projects/guix-channel/patches/wio-view-map-listener-leak-0.20.patch")
-		      (local-file
-			"/home/mnlcz/Projects/guix-channel/patches/wio-hide-0.20.patch")))))
+                      (local-file
+                       "/home/mnlcz/Projects/guix-channel/patches/wio-hide-0.20.patch")))))
     (build-system meson-build-system)
     (arguments
      (list
